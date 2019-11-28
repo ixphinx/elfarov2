d0ad5ec9d62784b8b16c523f51841d2e969759a3
ixphinx <riktmp@gmail.com>
2019-11-28 21:00:26 +0000

    desarrollo

--- src/index.js
+++ src/index.js
@@ -2,19 +2,44 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const morgan = require('morgan');
+const { mongodb } = require('./keys');
 
 //Configuracion
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'ejs');
+require('./passport/local-auth');
 
 //Server
-mongoose.connect('mongodb+srv://admin:huevon33@database-aizqn.gcp.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(()=> console.log('Database OK'))
+mongoose.set('useFindAndModify', false);
+mongoose.connect('mongodb+srv://admin:huevon33@database-aizqn.gcp.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Database OK'))
     .catch(e => console.log(e));
 
-//Middlewares
 
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+    secret: 'mysecretsession',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+    app.locals.signinMessage = req.flash('signinMessage');
+    app.locals.signupMessage = req.flash('signupMessage');
+    app.locals.user = req.user;
+    console.log(app.locals)
+    next();
+});
 
 //Rutas
 app.use(require('./routes/index'));
@@ -24,6 +49,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 //Run dev
-app.listen(app.get('port'), ()=>{
+app.listen(app.get('port'), () => {
     console.log('Servidor conectado en puerto: ' + app.get('port'));
 });
--- src/models/database.js
+++ src/models/database.js
@@ -1,8 +1,19 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
-const artista = Schema({
-    nombre: String,
-    likes: Number,
-    donations: Number
-})
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: String,
+  password: String
+});
+
+userSchema.methods.encryptPassword = (password) => {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+};
+
+userSchema.methods.comparePassword= function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model('user', userSchema);
